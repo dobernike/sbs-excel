d3396f076b93b274cee688bcc764d2f1ac1f03dd
dobernike <Dobernike@mail.ru>
2020-05-11 22:52:38 +0300

    55

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -2,7 +2,7 @@ import {$} from '@core/dom'
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
 import {resizeHandler} from './table.resize'
-import {shouldResize, isCell} from './table.functions'
+import {shouldResize, isCell, matrix} from './table.functions'
 import {TableSelection} from './TableSelection'
 
 export class Table extends ExcelComponent {
@@ -34,7 +34,13 @@ export class Table extends ExcelComponent {
       resizeHandler(this.$root, event)
     } else if (isCell(event)) {
       const $target = $(event.target)
-      this.selection.select($target)
+      if (event.shiftKey) {
+        const $cells = matrix($target, this.selection.current)
+            .map(id => this.$root.find(`[data-id="${id}"]`))
+        this.selection.selectGroup($cells)
+      } else {
+        this.selection.select($target)
+      }
     }
   }
 }

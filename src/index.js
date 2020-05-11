898bd5f5cb004a1fef26e24685ac8018cf684a77
dobernike <Dobernike@mail.ru>
2020-05-11 10:31:57 +0300

    30

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import './scss/index.scss';
+import './scss/index.scss'
+import {Excel} from '@/components/excel/Excel'
 
-console.log('Work');
+const excel = new Excel('#app', {
+  components: [],
+})
+
+console.log('Excel', excel)

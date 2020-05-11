a13bcceffa7f9be062515b8b7dee2f4443669c0e
dobernike <Dobernike@mail.ru>
2020-05-11 16:57:20 +0300

    45

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -21,13 +21,16 @@ export class Table extends ExcelComponent {
       // const $parent = $resizer.$el.parentNode // bad
       // const $parent = $resizer.$el.closest('.column') // better but bad
       const $parent = $resizer.closest('[data-type="resizable"]')
-
       const coords = $parent.getCoords()
+      console.log($parent.data.col);
+
 
       document.onmousemove = e => {
         const delta = e.pageX - coords.right
         const value = coords.width + delta
         $parent.$el.style.width = value + 'px'
+        document.querySelectorAll(`[data-col="${$parent.data.col}"]`)
+            .forEach(el => el.style.width = value + 'px')
       }
 
       document.onmouseup = () => {

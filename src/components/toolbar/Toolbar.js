858ebf270fb551c67d4d9f25ae648b3da5a8090b
dobernike <Dobernike@mail.ru>
2020-05-13 22:15:29 +0300

    Finish table logic

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -6,7 +6,7 @@ export class Toolbar extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Toolbar',
-      listeners: ['click'],
+      listeners: [],
       ...options
     })
   }
@@ -49,9 +49,5 @@ export class Toolbar extends ExcelComponent {
       </span>
     </div>
     `
-  }
-
-  onClick(event) {
-    console.log(event.target);
   }
 }

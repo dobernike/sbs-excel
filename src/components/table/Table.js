5ee2b9d0e817a7e96b1234695081acde2a37655b
dobernike <Dobernike@mail.ru>
2020-05-14 21:00:51 +0300

    6.6

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -20,7 +20,7 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return createTable(ROWS_COUNT)
+    return createTable(ROWS_COUNT, this.store.getState())
   }
 
   prepare() {

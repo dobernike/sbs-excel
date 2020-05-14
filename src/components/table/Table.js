29db5ca966920cef72f4ec6cbb1245f8c447908c
dobernike <Dobernike@mail.ru>
2020-05-14 22:38:23 +0300

    6.9

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -40,10 +40,6 @@ export class Table extends ExcelComponent {
     this.$on('formula:done', () => {
       this.selection.current.focus()
     })
-
-    // this.$subscribe(state => {
-    //   console.log('TableState', state)
-    // })
   }
 
   selectCell($cell) {

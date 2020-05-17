15c8396266f3b5ad1ead2869cad84c2195b2f793
dobernike <dobernike@mail.ru>
2020-05-17 10:13:41 +0300

    6.16

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ import {Toolbar} from '@/components/toolbar/Toolbar'
 import {Formula} from '@/components/formula/Formula'
 import {Table} from '@/components/table/Table'
 import {createStore} from '@core/createStore';
-import {storage} from '@core/utils';
+import {debounce, storage} from '@core/utils';
 import {rootReducer} from '@/redux/rootReducer';
 import {initialState} from '@/redux/initialState';
 import './scss/index.scss'
 
 const store = createStore(rootReducer, initialState)
 
-store.subscribe(state => {
+const stateListener = debounce(state => {
   console.log('App State: ', state)
   storage('excel-state', state)
-})
+}, 300)
+
+store.subscribe(stateListener)
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
